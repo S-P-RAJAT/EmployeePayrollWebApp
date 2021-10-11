3b8a506987bfc39edf894121de36e9931212fd79
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-12 03:06:47 +0530

    ADDED: Event listener to salry range to display output

---
+++ js/payroll_form.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input',function () {
+        output.textContent = salary.value; 
+    });
+    
+    });

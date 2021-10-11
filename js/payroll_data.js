f22fc83215834e8ec6ad3e12d6c86425c5ad9a76
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-12 03:53:39 +0530

    REFACTORED: To populate employee payroll data object on form submit

---
+++ js/payroll_data.js
@@ -0,0 +1,106 @@
+class EmployeePayRollData{
+
+    get id()
+    {
+        return this._id;
+    }
+
+    set id(id)
+    {
+        let idRegex = RegExp('[1-9]{1}[0-9]*');
+        if(idRegex.test(id))
+        this._id = id;
+        else
+        throw 'Id is incorrect';
+    }
+
+    get name()
+    {
+        return this._name;
+    }
+
+    set name(name)
+    {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if(nameRegex.test(name))
+        this._name = name;
+        else 
+        throw 'Name is incorrect';
+    }
+
+    get profilePic()
+    {
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic)
+    {
+        this._profilePic = profilePic;
+    }
+
+    get gender()
+    {
+        return this._gender;
+    }
+
+    set gender(gender)
+    {
+        this._gender = gender;
+    }
+
+    get department()
+    {
+        return this._department;
+    }
+
+    set department(department)
+    {
+        this._department = department;
+    }
+
+    get salary()
+    {
+        return this._salary;
+    }
+
+    set salary(salary)
+    {
+        this._salary = salary;
+    }
+
+    get note()
+    {
+        return this._note;
+    }
+
+    set note(note)
+    {
+        this._note = note;
+    }
+
+    get startDate()
+    {
+        return this._startDate;
+    }
+
+    set startDate(startDate)
+    {
+        let datee = new Date();
+        if(startDate<=datee)
+        {
+        this._startDate = startDate;
+        }
+        else
+        throw 'StartDate is incorrect';
+    }
+
+    toString()
+    {
+        const format = {year:'numeric', month:'long', day:'numeric'};
+        const date = this.startDate === undefined ? "undefined" :
+                     this.startDate.toLocaleDateString("en-US",format);
+        return "Name = "+this.name+", Gender = "+this.gender+", ProfilePic = \""+this.profilePic+"\", Department = ["+this.department+"], Salary = "+this.salary+
+                ", StartDate = "+date+", Note = \""+this.note+"\"";
+    }
+
+} 
--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,9 +1,51 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) => {
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
-    salary.addEventListener('input',function () {
-        output.textContent = salary.value; 
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
     });
-    
+
+});
+
+const save = () => {
+    let employeePayrollData;
+    try {
+        employeePayrollData = createEmployeePayroll();
+    } catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayRollData();
+    employeePayrollData.name = getInputValueById('#name');
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let year = getInputValueById('#year');
+    let month = parseInt(getInputValueById('#month')) - 1;
+    let day = getInputValueById('#day');
+    employeePayrollData.startDate = new Date(year, month, day);
+    console.log(employeePayrollData.toString());
+
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selectedItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selectedItems.push(item.value);
     });
+    return selectedItems;
+}

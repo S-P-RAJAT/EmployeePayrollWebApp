85f515cfc12809af4e21beeb1e08e84e818a600b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-14 01:33:17 +0530

    REFACTORED: Separate function to populate department details

--- js/payroll_home.js
+++ js/payroll_home.js
@@ -5,20 +5,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {
     const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
     let empPayrollData = createEmployeePayrollJSON()[0];
-    const innerHtml = ` ${headerHtml}
+    innerHtml = `${headerHtml}
         <tr>
-            <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+            <td><img class="profile"  src="${empPayrollData._profilePic}" alt=""></td>
             <td>${empPayrollData._name}</td>
             <td>${empPayrollData._gender}</td>
-            <td>
-                <div class="dept-label">${empPayrollData._department[0]}</div>
-                <div class="dept-label">${empPayrollData._department[1]}</div>
-            </td>
+            <td>${(getDeptHtml(empPayrollData._department))}</td>
             <td>${empPayrollData._salary}</td>
             <td>${empPayrollData._startDate}</td>
             <td>
-                <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                <img name="${empPayrollData._id}"" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+                <img name="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img name="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
             </td>
         </tr>
         `;
@@ -54,4 +51,12 @@ const createEmployeePayrollJSON = () => {
         }
     ];
     return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+    }
+    return deptHtml;
 }

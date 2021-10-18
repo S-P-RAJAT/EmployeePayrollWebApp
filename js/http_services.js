06431007ba2cdd81ab7dfd7ec8773ac400b45456
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-10-19 00:13:11 +0530

    ADDED: Simple html file to test REST services with JSON server

---
+++ js/http_services.js
@@ -0,0 +1,29 @@
+function makeServiceCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            if (xhr.status.toString().match('^[2][0-9]{2}$')) {
+                resolve(xhr.responseText);
+            } else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR Failed");
+            }
+        }
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhttp.statusText
+            });
+        };
+        xhr.open(methodType, url, async);
+        if (data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        } else xhr.send();
+        console.log(methodType + " request sent to the server");
+    });
+}
